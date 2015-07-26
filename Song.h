dea668e43bbd1ffa621a7d586a5d471213779711
Tom <tom@mcclenahan.me>
2015-07-26 02:18:19 -0500

    Better song

--- Song.h
+++ Song.h
@@ -9,11 +9,87 @@ const uint8_t scale[5] =
 
 const midi_chord song[] =
 {
-  {mC4, mNone},
-  {mD4, mNone},
-  {mE4, mNone},
   {mG4, mNone},
-  {mA4, mNone}
+  {mA4, mNone},
+
+  {mF4, mNone},
+  {mF5, mNone},
+
+  {mE5, mNone},
+  {mD5, mNone},
+  {mC5, mNone},
+  {mD5, mNone},
+  {mC5, mNone},
+  {mD5, mNone},
+
+  {mE5, mNone},
+  {mD5, mNone},
+  {mC5, mNone},
+  {mD5, mNone},
+  {mC5, mNone},
+  {mD5, mNone},
+  {mC5, mNone},
+  {mD5, mNone},
+
+  {mE5, mNone},
+  {mD5, mNone},
+  {mC5, mNone},
+  {mD5, mNone},
+  {mC5, mNone},
+  {mG4, mNone},
+  {mA4, mNone},
+
+  {mF4, mNone},
+  {mF5, mNone},
+
+  {mE5, mNone},
+  {mD5, mNone},
+  {mC5, mNone},
+  {mD5, mNone},
+  {mC5, mNone},
+  {mD5, mNone},
+
+  {mE5, mNone},
+  {mD5, mNone},
+  {mC5, mNone},
+  {mD5, mNone},
+  {mC5, mNone},
+  {mD5, mNone},
+  {mC5, mNone},
+  {mD5, mNone},
+
+
+  {mE5, mNone},
+  {mD5, mNone},
+  {mE5, mNone},
+  {mF5, mNone},
+  {mG5, mNone},
+  {mG5, mNone},
+  {mA5, mNone},
+  {mF5, mNone},
+
+  {mF5, mNone},
+  {mG5, mNone},
+  {mF5, mNone},
+  {mG5, mNone},
+  {mF5, mNone},
+  {mG5, mNone},
+
+  {mC5, mNone},
+  {mD5, mNone},
+  {mC5, mNone},
+  {mD5, mNone},
+  {mC5, mNone},
+  {mD5, mNone},
+
+  {mG4, mNone},
+  {mG4, mNone},
+  {mA4, mNone},
+  {mC5, mNone},
+  {mD5, mNone},
+  {mA4, mNone},
+  {mF4, mNone},
+
 };
 
 const int songLength = sizeof(song) / sizeof(midi_chord);
